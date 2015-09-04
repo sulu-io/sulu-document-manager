e3377841a5ef25247de90c01cbe412a4e7c17802
dantleech <dan.t.leech@gmail.com>
2015-09-04 17:50:40 +0200

    Register node types

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -177,13 +177,14 @@ class Bootstrap
         $session = $repository->login($credentials, 'default');
 
         $nodeTypeManager = $session->getWorkspace()->getNodeTypeManager();
-        if (!$nodeTypeManager->hasNodeType('mix:test')) {
-            $nodeTypeManager->registerNodeTypesCnd(<<<EOT
-[mix:test] > mix:referenceable mix
-[mix:mapping5] > mix:referenceable mix
-[mix:mapping10] > mix:referenceable mix
+
+        foreach (['mix:issue', 'mix:test', 'mix:mapping5', 'mix:mapping10'] as $mixinType) {
+            if (!$nodeTypeManager->hasNodeType($mixinType)) {
+                $nodeTypeManager->registerNodeTypesCnd(sprintf(<<<EOT
+    [%s] > mix:referenceable mix
 EOT
-            , true);
+                , $mixinType), true);
+            }
         }
 
         $namespaceRegistry = $session->getWorkspace()->getNamespaceRegistry();
